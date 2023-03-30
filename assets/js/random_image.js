e906c53e1c37a16c636f60e317375ffadd8d444a
vornex <79860992+vornex-gh@users.noreply.github.com>
2023-03-29 23:16:22 -0400

    Add files via upload

---
+++ assets/js/random_image.js
@@ -0,0 +1,1 @@
+window.onload=displayImage;var imagesArray=["avatar2.jpg","avatar3.jpg","cat.jpg","ciekawsze.jpg","swinia.jpg","anime4.gif","niewiem.gif","killerek.gif","saulgoodman.jpg"];function displayImage(){var num=Math.floor(Math.random()*(imagesArray.length));document.getElementById("main_image1").src="assets/img/animes/"+imagesArray[num];}
